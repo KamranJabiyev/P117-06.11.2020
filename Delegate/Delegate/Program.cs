f9740f0b27ce7666d53531df4279163e3bff2fa0
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-11-06 12:30:36 +0400

    Add files via upload

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        //public delegate bool Check(int n);
+        public delegate bool Check<T>(T n);
+        //public delegate void Write(string s);
+        public delegate void Write<T>(T obj);
+        public delegate U Test<in T,in M,out U>(T obj,M obj2);
+        static void Main(string[] args)
+        {
+            #region Delegate
+            int[] arr = { 5, 10, 15, 20, 25 };
+            //Console.WriteLine(Sum(arr,IsOdd));
+            //Console.WriteLine(Sum(arr,IsEven));
+            //Console.WriteLine(Sum(arr, IsElder));
+            #region Anonimous function,lambda expression
+            //Write write = new Write(Info);
+            //Write write = Info;
+            //write += GetUpper;
+            //write += GetLower;
+            //write += GetFirstSimbol;
+            //write -= GetLower;
+            //write += delegate (string str) 
+            //{
+            //    Console.WriteLine("Anonimous function: "+str);
+            //};
+
+            //write += str => Console.WriteLine("Anonimous function: " + str);
+
+            //write += delegate (string str)
+            //  {
+            //      Console.WriteLine("Anonimous function-2: "+str);
+            //  };
+            //write += str =>
+            //{
+            //    string result = str.Trim();
+            //    Console.WriteLine(result);
+            //};
+            //write -= delegate (string str)
+            //{
+            //    Console.WriteLine("Anonimous function-2: " + str);
+            //};
+            //write("Mustafa");
+
+            //Check check = IsOdd;
+            //check += IsEven;
+            //check += n => n > 15;
+            //Console.WriteLine(check(10));
+
+            //List<int> list = new List<int>();
+            //list.Add(1);
+            //list.Add(10);
+            //list.Add(100);
+            //int res=list.Find(n=>n==100);
+            //Console.WriteLine(res);
+            //Write<string> wStr = new Write<string>(Info);
+            //Write<int> wInt = n => Console.WriteLine(n);
+            //wStr("Mustafa");
+            //wInt(10);
+            #endregion
+
+            #region Predicate,Action,Func
+            //Predicate<int> predicate = IsOdd;
+            //predicate += n => n / 3 == 0;
+            //Console.WriteLine(predicate(11));
+
+            //Action<string> action = Info;
+            //action += GetUpper;
+            //action += GetLower;
+            //action -= GetLower;
+            //action += s => Console.WriteLine(s);
+            //action("Mustafa");
+
+            //Action<string, string> action1 = (s1, s2) => Console.WriteLine(s1+" "+s2);
+            //action1("Mustafa", "Ehmedov");
+
+            //Func<int> func = delegate ()
+            //{
+            //    return 5;
+            //};
+            //Console.WriteLine(func());
+            //Func<string,string,bool> func1 = (s1, s2)=>
+            //{
+            //    return s1.Length > s2.Length;
+            //};
+            //Console.WriteLine(func1("Mustafa","Ehmedov"));
+            #endregion
+            #endregion
+
+            #region Event
+            Student s1 = new Student("Mustafa", 99);
+            Student s2 = new Student("Melek", 64);
+            s1.Notify += point =>
+            {
+                if (point >= 65)
+                {
+                    Console.WriteLine("Eee gel diplomuvu gotur");
+                    return;
+                }
+                Console.WriteLine("Heyif");
+            };
+
+            s2.Notify += point =>
+            {
+                if (point >= 65)
+                {
+                    Console.WriteLine("Tebrikleeeeeeer.Bu qizdan yoxdu daaa...Code Academy belesini gormuyub");
+                    return;
+                }
+                Console.WriteLine("Biz Meleye layiq deyildik");
+            };
+
+            s1.SendMessage();
+            s2.SendMessage();
+            #endregion
+        }
+
+        #region Anonimous function,lambda expression
+        public static void Info(string str)
+        {
+            Console.WriteLine("Length: "+str.Length);
+        }
+
+        public static void GetUpper(string s)
+        {
+            Console.WriteLine("Upper: "+s.ToUpper());
+        }
+
+        public static void GetLower(string s)
+        {
+            Console.WriteLine("Lower: " + s.ToLower());
+        }
+
+        public static void GetFirstSimbol(string s)
+        {
+            Console.WriteLine("First Simbol: " + s[0]);
+        }
+        #endregion
+
+        #region Delegate
+        public static int Sum(int[] arr, Predicate<int> func)
+        {
+            int result = 0;
+            foreach (int num in arr)
+            {
+                if (func(num))
+                    result += num;
+            }
+
+            return result;
+        }
+
+        public static bool IsOdd(int n)
+        {
+            //Console.WriteLine("IsOdd");
+            return n % 2 != 0;
+        }
+
+        public static bool IsEven(int n)
+        {
+            //Console.WriteLine("IsEven");
+            return n % 2 == 0;
+        }
+
+        public static bool IsElder(int n)
+        {
+            return n > 10;
+        }
+        #endregion
+
+        #region Event
+        class Student
+        {
+            public string Name { get; set; }
+            public int Point { get; set; }
+            public event Action<int> Notify;
+
+            public Student(string name,int point)
+            {
+                Name = name;
+                Point = point;
+            }
+
+            public void SendMessage()
+            {
+                Notify(Point);
+                //if (Point >= 65)
+                //{
+                //    Console.WriteLine("Tebrikler");
+                //    return;
+                //}
+                //Console.WriteLine("Nc Nc Nc");
+            }
+        }
+        #endregion
+    }
+}
---
+++ StructGenericType/StructGenericType/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructGenericType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Struct
+            //Area area1 = new Area(20,10);
+            //area1.A = 200;
+            //Console.WriteLine(area1.GetArea());
+            //Area area;
+            //area.Test = 10;
+            //Console.WriteLine(area.Test);
+            #endregion
+
+            #region GenericType
+            //MyStrList strList = new MyStrList();
+            //strList.AddMember("Mustafa");
+            //strList.AddMember("Mustafa1");
+            //strList.AddMember("Mustafa2");
+            //Console.WriteLine(strList.GetMember(0));
+
+            //MyIntList intList = new MyIntList();
+            //intList.AddMember(1);
+
+            //MyList<string> strList = new MyList<string>();
+            //strList.AddMember("Mustafa");
+            //strList.AddMember("Mustafa1");
+            //string str = strList.GetMember(0);
+
+            //MyList<int> intList = new MyList<int>();
+            //intList.AddMember(1);
+
+            //MyList<Person> pList = new MyList<Person>();
+            //pList.AddMember(new Person());
+            //MyList<ICan> myList = new MyList<ICan>();
+
+            //Write<int>(10);
+            //Write<string>("Mustafa");
+            //Test<string, string>("Nahide", "Memishova");
+            //MyList<Student, Person> myList = new MyList<Student, Person>();
+            //MyList<Person, ICan> myList1 = new MyList<Person, ICan>();
+            //MyList<string, object> myList2 = new MyList<string, object>();
+            #endregion
+
+            #region Collection
+            #region ArrayList
+            //ArrayList list = new ArrayList();
+            //list.Add(5);
+            //list.Add(true);
+            //list.Add('a');
+            //list[5] = 10;
+            //list.Clear();
+            //Console.WriteLine(list.Contains(false));
+            //list.Remove(true);
+            //Console.WriteLine(list.Count);
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region List
+            //List<string> mylist = new List<string>();
+            //mylist.Add("Mustafa");
+            //mylist.Add("Mustafa1");
+            //mylist.Add("Mustafa2");
+            ////mylist.RemoveAt(2);
+            //mylist[0] = "Elgun1";
+            //foreach (string item in mylist)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Dictionary
+            //Dictionary<string, string> phones = new Dictionary<string, string>();
+            //phones.Add("Kamran", "+994512373434");
+            //phones.Add("Elgun", "+994558835613");
+
+            //phones["Mehemmed"] = "+994775705780";
+            //foreach (var item in phones)
+            //{
+            //    Console.WriteLine($"{item}");
+            //}
+
+            //Console.WriteLine(phones["Kamran"]);
+
+            #endregion
+
+            #region Queue - FIFO
+            //Queue<string> q = new Queue<string>();
+            //q.Enqueue("Melek");
+            //q.Enqueue("Nahide");
+            //q.Enqueue("Turkan");
+            //string deq = q.Dequeue();
+            //Console.WriteLine(deq);
+            //Console.WriteLine("Siradakilar:");
+            //foreach (string item in q)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Stack - LIFO
+            //Stack<string> stack = new Stack<string>();
+            //stack.Push("Turkan");
+            //stack.Push("Nahide");
+            //stack.Push("Melek");
+
+            ////Console.WriteLine(stack.Peek());
+            //stack.Pop();
+
+            //Console.WriteLine("Siradakilar:");
+            //foreach (string item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #endregion
+        }
+        #region GenericType
+        public static void Write<GT>(GT n)
+        {
+            Console.WriteLine(n);
+        }
+
+        public static void Test<T,U>(in T n1,U n2)
+        {
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+        }
+
+        #endregion
+    }
+
+    #region GenericType
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="U"></typeparam>
+    //class MyList<T> where T:class,ICan,new()
+    class MyList<T,U> where T: U
+
+    {
+        private T[] myArr;
+        public MyList()
+        {
+            myArr = new T[0];
+        }
+
+        public void AddMember(T member)
+        {
+            Array.Resize(ref myArr, myArr.Length + 1);
+            myArr[myArr.Length - 1] = member;
+        }
+
+        public T GetMember(int index)
+        {
+            return myArr[index];
+        }
+    }
+
+
+    class Person:ICan
+    {
+
+    }
+
+    class Student : Person
+    {
+
+    }
+    #endregion
+
+    #region Struct - value 
+    interface ICan
+    {
+
+    }
+    struct Area:ICan
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public int Test;
+        public Area(double a,double b)
+        {
+            A = a;
+            B = b;
+            Test = 10;
+        }
+
+        public double GetArea()
+        {
+            return A * B;
+        }
+    }
+
+    #endregion
+}
